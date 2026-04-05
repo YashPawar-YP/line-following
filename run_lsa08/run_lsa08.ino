2f57459e6be66d5ee4fe2df42f7f8c692609d53a
hsrkl <hrishikesh_l@outlook.com>
2026-04-05 19:53:15 +0530

    Reading code stored in run_lsa08, PID not implemented yet

--- run_lsa08/run_lsa08.ino
+++ run_lsa08/run_lsa08.ino
@@ -1,50 +1,43 @@
 #include <SoftwareSerial.h>
 
-SoftwareSerial lsaSerial(11, 12);
+SoftwareSerial lsaSerial(4, 7);
 
-const int UEN_PIN = /* insert pin here */ ;  // Connect to LSA08 UEN pin; pull LOW to stream data
+bool readRawSensors(int* s) {
+  while (lsaSerial.available()) lsaSerial.read();  // flush stale bytes
 
-int sensorValues[8];   // Raw analog value 0-255 for each of the 8 sensors
+  unsigned long t = millis();
+  while (millis() - t < 150) {
+    if (lsaSerial.available() && lsaSerial.read() == 0x00) goto read_bytes;
+  }
+  return false;
 
-bool readRawSensors() {
-    // UART Mode 3 packet format: [0x00][S0][S1][S2][S3][S4][S5][S6][S7]
-    unsigned long t = millis();
-    while (true) {
-        if (millis() - t > 100) return false;  // timeout
-        if (lsaSerial.available() && lsaSerial.peek() == 0x00) {
-            lsaSerial.read();
-            break;
-        } else if (lsaSerial.available()) {
-            lsaSerial.read();
-        }
-    }
-
-    // Read 8 sensor bytes
+read_bytes:
+  t = millis();
+  for (int i = 0; i < 8; i++) {
+    while (!lsaSerial.available()) if (millis() - t > 50) return false;
+    s[i] = lsaSerial.read();
     t = millis();
-    while (lsaSerial.available() < 8) {
-        if (millis() - t > 50) return false;
-    }
-    for (int i = 0; i < 8; i++) {
-        sensorValues[i] = (int) lsaSerial.read();  // integer 0-255
-    }
-    return true;
+  }
+  return true;
 }
 
 void setup() {
-    Serial.begin(9600);
-    lsaSerial.begin(19200);  // Match LSA08 BAUDRATE setting (38400 = setting 2)
-
-    pinMode(UEN_PIN, OUTPUT);
-    digitalWrite(UEN_PIN, LOW);  // Pull UEN low to enable streaming
+  Serial.begin(115200);
+  lsaSerial.begin(9600);
+  delay(200);
+  Serial.println("reddy");
 }
 
 void loop() {
-    if (readRawSensors()) {
-        for (int i = 0; i < 8; i++) {
-            Serial.print("S"); Serial.print(i);
-            Serial.print("="); Serial.print(sensorValues[i]);
-            if (i < 7) Serial.print("  ");
-        }
-        Serial.println();
+  int s[8];
+  if (readRawSensors(s)) {
+    for (int i = 0; i < 8; i++) {
+      Serial.print("S"); Serial.print(i);
+      Serial.print("="); Serial.print(s[i]);
+      if (i < 7) Serial.print("  ");
     }
+    Serial.println();
+  } else {
+    Serial.println("tejas mandhare");
+  }
 }
